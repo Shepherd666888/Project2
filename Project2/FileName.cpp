86d0a90ffad28d003ed93f18c6d0a06d0dc80385
Shepherd-Six <2036484293@qq.com>
2026-09-14 20:54:25 +0800

    面向对象——矩形面积练习

--- Project2/FileName.cpp
+++ Project2/FileName.cpp
@@ -1,15 +1,62 @@
 #include<iostream>
 using namespace std;
-class Practice{
+//class Clock{
+//private:
+//	int hour;
+//	int minute;
+//	int seconde;
+//public:
+//	Clock();
+//	Clock(int h);
+//	//void setHour(int h);
+//	void showHour();
+//	~Clock();
+//};
+////void Clock::setHour(int h) {
+////	hour = h;
+////}
+//void Clock::showHour() {
+//	cout << hour << endl;
+//}
+//Clock::Clock() {
+//	hour = 12;
+//	minute = 30;
+//	seconde = 15;
+//	cout << hour << minute << seconde;
+//}
+//Clock::Clock(int h) {
+//	hour = h;
+//	cout << hour<<" "<< minute << endl;
+//}
+//Clock::~Clock() {
+//
+//}
+//class ClockWithData :public Clock {
+//
+//};
+//int main() {
+//	Clock myclock,myclock1(12);
+class Rectangle{
+private:
+	int length;
+	int width;
+	int s=0;
 public:
-	Practice() {
-		cout << "hello"<<endl;
-	}
-	~Practice() {
-		cout << "?";
-	}
+	Rectangle(int len,int wid);
+	int getS();
 };
-int main() {
-	Practice stu;
+Rectangle::Rectangle(int len,int wid) {
+	length = len;
+	width = wid;
+}
+int Rectangle::getS() {
+	s = length * width ;
+	return s;
+}
+int main(){
+	int len, wid;
+	cin >> len >> wid;
+	Rectangle s(len, wid);
+	cout<<"矩形面积是："<<s.getS();
 	return 0;
 }
